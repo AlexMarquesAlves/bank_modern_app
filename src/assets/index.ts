82ee24a6a03bea6b355ff11c08abeba0b6b3dbbb
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-08-16 13:45:27 -0300

    Feat: improving imports

---
+++ src/assets/index.ts
@@ -0,0 +1,31 @@
+import airbnb from "./airbnb.png";
+import binance from "./binance.png";
+import coinbase from "./coinbase.png";
+import dropbox from "./dropbox.png";
+import facebook from "./facebook.svg";
+import instagram from "./instagram.svg";
+import linkedin from "./linkedin.svg";
+import people01 from "./people01.png";
+import people02 from "./people02.png";
+import people03 from "./people03.png";
+import send from "./Send.svg";
+import shield from "./Shield.svg";
+import star from "./Star.svg";
+import twitter from "./twitter.svg";
+
+export {
+  airbnb,
+  binance,
+  coinbase,
+  dropbox,
+  facebook,
+  instagram,
+  linkedin,
+  people01,
+  people02,
+  people03,
+  send,
+  shield,
+  star,
+  twitter,
+};
