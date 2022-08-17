8148c2f03ad7263a224cf2b6d067318a79829325
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-08-17 00:05:42 -0300

    Feat: Organized Structure & Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,45 @@
+import {
+  Billing,
+  Business,
+  CardDeal,
+  Clients,
+  CTA,
+  Footer,
+  Hero,
+  Navbar,
+  Stats,
+  Testimonials,
+} from "./Components";
 import styles from "./Styles";
 
 export const App = () => {
   return (
     <div className="bg-black w-full overflow-hidden">
-      <div className={`${styles.paddingX} ${styles.flexCenter}`}></div>
+      {/* Navbar Section */}
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+      {/* Hero Section */}
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+      {/*  Sections */}
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
     </div>
   );
 };
