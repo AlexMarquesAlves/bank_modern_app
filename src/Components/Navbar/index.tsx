1fd87d9297067c36ada9ae64cb19cbacd57dc5aa
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-08-17 00:54:19 -0300

    Feat: Added a list on desktop view

--- src/Components/Navbar/index.tsx
+++ src/Components/Navbar/index.tsx
@@ -1,10 +1,24 @@
 // import { useState } from "react";
 import { logo } from "../../assets";
+import { navLinks } from "../../Constants";
 
 export const Navbar = () => {
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
       <img src={logo} alt="hoobank" className="w-[7.75rem] h-[2rem]" />
+
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-[1rem] ${
+              index === navLinks.length - 1 ? "mr-0" : "mr-10"
+            } text-white`}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 };
