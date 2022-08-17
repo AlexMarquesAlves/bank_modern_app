2aa2af56cd733b6b491eadf833a9587533ba648d
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-08-17 01:10:45 -0300

    Feat: Added a list on mobile view

--- src/Components/Navbar/index.tsx
+++ src/Components/Navbar/index.tsx
@@ -1,12 +1,15 @@
-// import { useState } from "react";
-import { logo } from "../../assets";
+import { useState } from "react";
+import { close, logo, menu } from "../../assets";
 import { navLinks } from "../../Constants";
 
 export const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
       <img src={logo} alt="hoobank" className="w-[7.75rem] h-[2rem]" />
 
+      {/* Desktop Links*/}
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
           <li
@@ -19,6 +22,35 @@ export const Navbar = () => {
           </li>
         ))}
       </ul>
+
+      {/* Mobile Links */}
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[1.75rem] h-[1.75rem] object-contain cursor-pointer"
+          onClick={() => setToggle((prev) => !prev)}
+        />
+
+        <div
+          className={`${
+            toggle ? "flex" : "hidden"
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[8.75rem] rounded-xl sidebar`}
+        >
+          <ul className="list-none flex flex-col justify-end items-center flex-1">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-normal cursor-pointer text-[1rem] ${
+                  index === navLinks.length - 1 ? "mr-0" : "mb-4"
+                } text-white`}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
