81f48dac2c19012847f609638d95600e3f1ea443
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-08-18 02:34:01 -0300

    Feat: 🆕 pink__gradient

--- src/Components/Hero/index.tsx
+++ src/Components/Hero/index.tsx
@@ -48,6 +48,8 @@ export const Hero = () => {
           alt="robot hand"
           className="w-[100%] h-[100%] relative z-[5]"
         />
+
+        <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient " />
       </div>
     </section>
   );
