10b0f63dfbab35f62fe73f8dd083c909b085ab0e
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-08-18 02:34:26 -0300

    Feat: 🆕 white__gradient

--- src/Components/Hero/index.tsx
+++ src/Components/Hero/index.tsx
@@ -50,6 +50,7 @@ export const Hero = () => {
         />
 
         <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient " />
+        <div className="absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient " />
       </div>
     </section>
   );
