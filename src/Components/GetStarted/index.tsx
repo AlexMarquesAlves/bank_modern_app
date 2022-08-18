6646291eae0cc7326aa80d7f816ab2d850eec701
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-08-18 02:03:19 -0300

    Feat: 🆕 bg gradient 🎨

--- src/Components/GetStarted/index.tsx
+++ src/Components/GetStarted/index.tsx
@@ -4,7 +4,9 @@ import { arrowUp } from "../../assets";
 export const GetStarted = () => {
   return (
     // bg gradient
-    <div>
+    <div
+      className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer `}
+    >
       {/* bg black */}
       <div>
         <div>
