9b41c5e343f1dadc325ec19d4a5e3b5bd83d78c9
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-08-18 13:57:50 -0300

    Feat: 🆕 element structure

--- src/Components/Business/index.tsx
+++ src/Components/Business/index.tsx
@@ -5,8 +5,17 @@ import { Button } from "../Button";
 
 export const Business = () => {
   return (
-    <>
-      <h1>Business</h1>
-    </>
+    <section id="features">
+      <div>
+        <h2>
+          You do the business, <br /> we'll handle the money.
+        </h2>
+        <p>
+          With the right credit card, you can improve your financial life by
+          building credit, earning rewards and saving money. But with hundreds
+          os credit cards on the market
+        </p>
+      </div>
+    </section>
   );
 };
