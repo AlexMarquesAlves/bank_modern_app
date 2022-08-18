004574cb549e09885fd837491f114e07871d0363
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-08-18 14:01:38 -0300

    Feat: 🆕 🎨 of paragraph

--- src/Components/Business/index.tsx
+++ src/Components/Business/index.tsx
@@ -11,7 +11,7 @@ export const Business = () => {
           You do the business, <br className="sm:block hidden" /> we'll handle
           the money.
         </h2>
-        <p>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
           With the right credit card, you can improve your financial life by
           building credit, earning rewards and saving money. But with hundreds
           os credit cards on the market
