08f3a33597dc034f44b50fcf431ee4511a3f42e5
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-08-18 13:35:51 -0300

    Feat: 🆕 heading four 🎨

--- src/Components/Stats/index.tsx
+++ src/Components/Stats/index.tsx
@@ -9,7 +9,9 @@ export const Stats = () => {
     >
       {stats.map((stat) => (
         <div key={stat.id} className="">
-          <h4 className="">{stat.value} </h4>
+          <h4 className="font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px] text-white">
+            {stat.value}{" "}
+          </h4>
           <p className="">{stat.title}</p>
         </div>
       ))}
