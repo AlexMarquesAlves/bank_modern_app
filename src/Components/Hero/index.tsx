269c94c99e1df5495a36fc454ee154b95d577e08
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-08-18 01:16:16 -0300

    Feat: Added discount element

--- src/Components/Hero/index.tsx
+++ src/Components/Hero/index.tsx
@@ -1,7 +1,23 @@
+import styles from "../../Styles";
+import { discount, robot } from "../../assets";
+
 export const Hero = () => {
   return (
-    <>
-      <h1>Hero</h1>
-    </>
+    <section
+      id="home"
+      className={`flex md:flex-row flex-col ${styles.paddingY}`}
+    >
+      <div
+        className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-16`}
+      >
+        <div className="flex flex-row items-center py-[0.375rem] px-4 bg-discount-gradient rounded-[0.625rem] mb-2 ">
+          <img src={discount} alt="Discount" className="w-[2rem] h-[2rem]" />
+          <p className={`${styles.paragraph} ml-2`}>
+            <span className="text-white ">20%</span> Discount For {` `}
+            <span className="text-white ">1 Month</span> Account
+          </p>
+        </div>
+      </div>
+    </section>
   );
 };
--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,6 +2,7 @@ import airbnb from "./airbnb.png";
 import binance from "./binance.png";
 import close from "./close.svg";
 import coinbase from "./coinbase.png";
+import discount from "./discount.svg";
 import dropbox from "./dropbox.png";
 import facebook from "./facebook.svg";
 import instagram from "./instagram.svg";
@@ -11,6 +12,7 @@ import menu from "./menu.svg";
 import people01 from "./people01.png";
 import people02 from "./people02.png";
 import people03 from "./people03.png";
+import robot from "./robot.svg";
 import send from "./Send.svg";
 import shield from "./Shield.svg";
 import star from "./Star.svg";
@@ -21,6 +23,7 @@ export {
   binance,
   close,
   coinbase,
+  discount,
   dropbox,
   facebook,
   instagram,
@@ -30,6 +33,7 @@ export {
   people01,
   people02,
   people03,
+  robot,
   send,
   shield,
   star,
