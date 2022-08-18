2bbd247edf080bd86f5f062eb4536a9fb02f9837
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-08-18 02:04:24 -0300

    Feat: 🆕bg black 🎨

--- src/Components/GetStarted/index.tsx
+++ src/Components/GetStarted/index.tsx
@@ -8,7 +8,9 @@ export const GetStarted = () => {
       className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer `}
     >
       {/* bg black */}
-      <div>
+      <div
+        className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full `}
+      >
         <div>
           <p>
             <span>Get</span>
