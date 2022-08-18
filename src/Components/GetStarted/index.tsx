0912059d11d98303941a9a8aad0592dad4f0bb5d
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-08-18 02:08:23 -0300

    Fix merge

--- src/Components/GetStarted/index.tsx
+++ src/Components/GetStarted/index.tsx
@@ -12,27 +12,16 @@ export const GetStarted = () => {
         className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full `}
       >
         <div className={`${styles.flexStart} flex-row `}>
-<<<<<<< HEAD
           <p className="font-poppins font-medium text-[1.125rem] leading-[1.4375rem] mr-2">
-=======
-          <p className="font-poppins font-medium text-[18px] leading-[23px] mr-2">
->>>>>>> 49bccbe6dff6371944a929694d670e9410839ecf
             <span className="text-gradient">Get</span>
           </p>
           <img
             src={arrowUp}
             alt="arrowUp"
-<<<<<<< HEAD
             className="w-[1.4375rem] h-[1.4375rem] object-contain "
           />
         </div>
         <p className="font-poppins font-medium text-[1.125rem] leading-[1.4375rem] ">
-=======
-            className="w-[23px] h-[23px] object-contain "
-          />
-        </div>
-        <p className="font-poppins font-medium text-[18px] leading-[23px] ">
->>>>>>> 49bccbe6dff6371944a929694d670e9410839ecf
           <span className="text-gradient">Started</span>
         </p>
       </div>
