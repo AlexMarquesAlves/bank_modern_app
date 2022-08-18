49bccbe6dff6371944a929694d670e9410839ecf
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-08-18 02:05:25 -0300

    Feat: 🆕 get started 🎨

--- src/Components/GetStarted/index.tsx
+++ src/Components/GetStarted/index.tsx
@@ -11,14 +11,18 @@ export const GetStarted = () => {
       <div
         className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full `}
       >
-        <div>
-          <p>
-            <span>Get</span>
+        <div className={`${styles.flexStart} flex-row `}>
+          <p className="font-poppins font-medium text-[18px] leading-[23px] mr-2">
+            <span className="text-gradient">Get</span>
           </p>
-          <img src={arrowUp} alt="arrowUp" />
+          <img
+            src={arrowUp}
+            alt="arrowUp"
+            className="w-[23px] h-[23px] object-contain "
+          />
         </div>
-        <p>
-          <span>Started</span>
+        <p className="font-poppins font-medium text-[18px] leading-[23px] ">
+          <span className="text-gradient">Started</span>
         </p>
       </div>
     </div>
