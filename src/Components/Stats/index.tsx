75ba5db8db7c4fbea37596132ff8055fc1cb00d1
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-08-18 13:34:40 -0300

    Feat: 🆕 Mapping stats

--- src/Components/Stats/index.tsx
+++ src/Components/Stats/index.tsx
@@ -1,9 +1,18 @@
 // imports
+import { stats } from "../../Constants";
+import styles from "../../Styles";
 
 export const Stats = () => {
   return (
-    <>
-      <h1>Stats</h1>
-    </>
+    <section
+      className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}
+    >
+      {stats.map((stat) => (
+        <div key={stat.id} className="">
+          <h4 className="">{stat.value} </h4>
+          <p className="">{stat.title}</p>
+        </div>
+      ))}
+    </section>
   );
 };
