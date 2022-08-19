278658dd34a0d0c3f53ee630b21cb83327cb942d
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-08-19 00:23:35 -0300

    Feat: Structure & Button 🎨

--- src/Components/Button/index.tsx
+++ src/Components/Button/index.tsx
@@ -4,8 +4,11 @@ export type IButtonProps = {
 
 export const Button = ({ styles }: IButtonProps) => {
   return (
-    <>
-      <h1>Button</h1>
-    </>
+    <button
+      type="button"
+      className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}
+    >
+      Get Started
+    </button>
   );
 };
