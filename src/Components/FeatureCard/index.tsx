f9785ed17e8d64b088cdd42ca74ecd85fb259361
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-08-19 00:49:39 -0300

    Feat: Updated icons 🎨

--- src/Components/FeatureCard/index.tsx
+++ src/Components/FeatureCard/index.tsx
@@ -1,4 +1,5 @@
 import { features } from "../../Constants";
+import styles from "../../Styles";
 
 export type IFeatureCardProps = {
   index: number;
@@ -14,8 +15,15 @@ export const FeatureCard = ({
   icon,
 }: IFeatureCardProps) => {
   return (
-    <div>
-      <div>
+    <div
+      className={`flex flex-row p-6 rounded-[20px] ${
+        index !== features.length - 1 ? "mb-6" : "mb-0"
+      } feature-card`}
+    >
+      {/* Icons */}
+      <div
+        className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}
+      >
         <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain" />
       </div>
     </div>
