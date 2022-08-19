f9bb40be5bd6abfe29056931736533090bbf5467
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-08-19 00:37:01 -0300

    Feat: Updated property type for FeatureCard

--- src/Components/FeatureCard/index.tsx
+++ src/Components/FeatureCard/index.tsx
@@ -1,8 +1,18 @@
+import { features } from "../../Constants";
+
 export type IFeatureCardProps = {
   index: number;
+  content: string;
+  title: string;
+  icon: string;
 };
 
-export const FeatureCard = ({ index }: IFeatureCardProps) => {
+export const FeatureCard = ({
+  index,
+  content,
+  title,
+  icon,
+}: IFeatureCardProps) => {
   return (
     <>
       <div>FeatureCard</div>
