1ee463f6163e204258e6ce9b4cd240a511596056
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-08-20 22:46:14 -0300

    Feat: 🆕 Apple store image button was added

--- src/Components/Billing/index.tsx
+++ src/Components/Billing/index.tsx
@@ -26,6 +26,14 @@ export const Billing = () => {
           autem doloribus corrupti, maiores totam pariatur eaque porro labore in
           veniam!
         </p>
+
+        <div className={"flex flex-row flex-wrap sm:mt-10 mt-6"}>
+          <img
+            src={apple}
+            alt="apple_store"
+            className={"w-[128px] h-[42px] object-contain mr-5 cursor-pointer"}
+          />
+        </div>
       </div>
     </section>
   );
