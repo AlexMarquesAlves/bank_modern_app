9e2276f3ee67cbcf1afb65110cac6f1165eea09e
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-08-21 22:32:36 -0300

    Feat: 🆕 section tag was added

--- src/Components/CardDeal/index.tsx
+++ src/Components/CardDeal/index.tsx
@@ -4,9 +4,5 @@ import styles, { layout } from "../../Styles";
 import { Button } from "../Button";
 
 export const CardDeal = () => {
-  return (
-    <>
-      <h1 className="text-center text-7xl">CardDeal</h1>
-    </>
-  );
+  return <section></section>;
 };
