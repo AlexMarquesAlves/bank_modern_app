1f0dbc376d416f15906ee8fa41f363c96a9b6a0c
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-08-21 22:37:33 -0300

    Feat: 🆕 heading styles

--- src/Components/CardDeal/index.tsx
+++ src/Components/CardDeal/index.tsx
@@ -7,7 +7,7 @@ export const CardDeal = () => {
   return (
     <section className={layout.section}>
       <div className={layout.sectionInfo}>
-        <h2>
+        <h2 className={styles.heading2}>
           Find a better card deal <br className="sm:block hidden" /> in few easy
           steps.
         </h2>
