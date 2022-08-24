696d3f3a750bf440a0fbfab4221ee46c814bf7d0
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-08-24 01:01:47 -0300

    Feat: 🆕 div styles updated

--- src/Components/Clients/index.tsx
+++ src/Components/Clients/index.tsx
@@ -5,9 +5,12 @@ import styles from "../../Styles";
 export const Clients = () => {
   return (
     <section className={`${styles.flexCenter} my-4`}>
-      <div className={`${styles.flexCenter}`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full`}>
         {clients.map((client) => (
-          <div key={client.id}>
+          <div
+            key={client.id}
+            className={`${styles.flexCenter} flex-1  sm:min-w-[192px] min-w-[120px]`}
+          >
             <img
               src={client.logo}
               alt="client"
