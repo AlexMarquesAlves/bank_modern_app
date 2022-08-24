73fe47b76fc139ccbd4fb05ab0892c1aef391583
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-08-24 00:17:31 -0300

    Feat: 🆕 image styles was added

--- src/Components/FeedbackCard/index.tsx
+++ src/Components/FeedbackCard/index.tsx
@@ -5,7 +5,11 @@ export const FeedbackCard = () => {
     <div
       className={`flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr- my5 feedback-card`}
     >
-      <img src={quotes} alt="quotes" />
+      <img
+        src={quotes}
+        alt="double_quotes"
+        className={`w-[42px] h-[27px] object-contain`}
+      />
     </div>
   );
 };
