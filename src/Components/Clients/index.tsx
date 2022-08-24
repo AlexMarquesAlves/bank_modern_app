2edfda5cc1124f22605a85e8d5e04b3b7f5aeb17
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-08-24 00:55:16 -0300

    Feat: 🆕 mapping data

--- src/Components/Clients/index.tsx
+++ src/Components/Clients/index.tsx
@@ -3,5 +3,13 @@ import { clients } from "../../Constants";
 import styles from "../../Styles";
 
 export const Clients = () => {
-  return <section className={`${styles.flexCenter} my-4`}></section>;
+  return (
+    <section className={`${styles.flexCenter} my-4`}>
+      <div className={`${styles.flexCenter}`}>
+        {clients.map((client) => (
+          <div key={client.id}></div>
+        ))}
+      </div>
+    </section>
+  );
 };
