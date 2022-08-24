3a422ee453df37675bed8d635fbb1abcd649dbbc
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-08-24 00:10:46 -0300

    Feat: 🆕 FeedbackCard component styles

--- src/Components/FeedbackCard/index.tsx
+++ src/Components/FeedbackCard/index.tsx
@@ -1,7 +1,7 @@
 export const FeedbackCard = () => {
   return (
-    <>
-      <h1>FeedbackCard</h1>
-    </>
+    <div
+      className={`flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr- my5 feedback-card`}
+    ></div>
   );
 };
