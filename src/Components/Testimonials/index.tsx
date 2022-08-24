040247c2dae6e7911519374999c49088fae6c30b
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-08-23 23:48:30 -0300

    Feat: 🆕 heading styles

--- src/Components/Testimonials/index.tsx
+++ src/Components/Testimonials/index.tsx
@@ -17,7 +17,7 @@ export const Testimonials = () => {
           "w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]"
         }
       >
-        <h1>
+        <h1 className={styles.heading2}>
           What people are <br className={"sm:block hidden"} />
           saying about us
         </h1>
