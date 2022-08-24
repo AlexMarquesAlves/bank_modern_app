079a9b370d5789968ab256143e6e0335b4fa2a05
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-08-24 00:12:59 -0300

    Feat:🆕 image element was added

--- src/Components/FeedbackCard/index.tsx
+++ src/Components/FeedbackCard/index.tsx
@@ -1,7 +1,11 @@
+import { quotes } from "../../assets";
+
 export const FeedbackCard = () => {
   return (
     <div
       className={`flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr- my5 feedback-card`}
-    ></div>
+    >
+      <img src={quotes} alt="quotes" />
+    </div>
   );
 };
