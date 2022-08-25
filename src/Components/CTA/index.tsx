9591ef5a6cfa013bbba310bda07b831e62fc6867
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-08-25 00:34:32 -0300

    Feat: 🆕 styles styles info updated

--- src/Components/CTA/index.tsx
+++ src/Components/CTA/index.tsx
@@ -7,7 +7,7 @@ export const CTA = () => {
     <section
       className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}
     >
-      <div className={``}>
+      <div className={`flex-1 flex flex-col`}>
         <h2 className={``}>Let's try our service now!</h2>
         <p className={``}>
           Everything you need to accept card payments and grow your business
