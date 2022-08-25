d89633dbe34ff49d36586a6e34bf61e8cd925722
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-08-25 01:01:52 -0300

    Feat: 🆕 image div styles added

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -7,7 +7,7 @@ export const Footer = () => {
   return (
     <section className={`${styles.flexCenter} ${styles.paddingY} flex-col `}>
       <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full `}>
-        <div>
+        <div className={`flex-1 flex flex-col justify-start mr-10`}>
           <img src={logo} alt="logo" />
         </div>
       </div>
