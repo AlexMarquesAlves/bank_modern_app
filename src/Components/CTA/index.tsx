166e2795d8c61e373b084472a55117de36b3bff3
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-08-25 00:33:18 -0300

    Feat: 🆕 section styles updated

--- src/Components/CTA/index.tsx
+++ src/Components/CTA/index.tsx
@@ -4,17 +4,19 @@ import { Button } from "../Button";
 
 export const CTA = () => {
   return (
-    <section>
-      <div>
-        <h2>Let's try our service now!</h2>
-        <p>
+    <section
+      className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}
+    >
+      <div className={``}>
+        <h2 className={``}>Let's try our service now!</h2>
+        <p className={``}>
           Everything you need to accept card payments and grow your business
           anywhere on planet.
         </p>
       </div>
 
-      <div>
-        <Button />
+      <div className={``}>
+        <Button styles={``} />
       </div>
     </section>
   );
