26bdf06fbf445bfbd5d46a606087f05b968fb050
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-08-25 01:01:09 -0300

    Feat: 🆕 image added

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -6,9 +6,11 @@ import { footerLinks, socialMedia } from "../../Constants";
 export const Footer = () => {
   return (
     <section className={`${styles.flexCenter} ${styles.paddingY} flex-col `}>
-      <div
-        className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full `}
-      ></div>
+      <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full `}>
+        <div>
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
     </section>
   );
 };
